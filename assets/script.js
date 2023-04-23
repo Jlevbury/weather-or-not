7fe39fbfc4382e11cfc1d43dc0a6b6d0c9d06fe6
Jlevbury <125767916+Jlevbury@users.noreply.github.com>
2023-04-23 17:30:05 -0400

    javascript fetch in development, figuring out how to parse information to later append

--- assets/script.js
+++ assets/script.js
@@ -5,24 +5,33 @@ let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + 
 
 //fetch(`${API_BASE_URL}weather?q=${cityName}&appid=${apiKey}`);
 //console.log(fetch);
-const submitBtn = document.querySelector("#submit");
-const locationInput = document.querySelector("#location");
+var submitBtn = document.querySelector("#submit");
+var locationInput = document.querySelector("#location");
 
 // Add event listener to the submit button
 submitBtn.addEventListener("click", () => {
+  
     // Get the value of the location input
-    const location = locationInput.value;
+    var location = locationInput.value;
     
     // Build the query URL with the location input value and API key
-    const apiKey = "826f633421f0289a9fa069f465862d53";
-    const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+    var apiKey = "826f633421f0289a9fa069f465862d53";
+    var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=imperial`;
     
     // Call the fetch function with the query URL
     fetch(queryURL)
       .then(response => response.json())
       .then(data => {
-        console.log(data);
-        // Append the data to the page for the user to view
+        temp = data.main.temp;
+        hum = data.main.humidity;
+        wind= data.wind.speed;
+        icon= data.weather[0].icon;
+        city = data.name;
+        console.log(wind);
+        console.log(hum);
+        console.log(temp);
+        console.log(icon);
+        console.log(city);
       })
       .catch(error => {
         console.error(error);
