2c6e5bb856a1097d98beb58dcf92162a8c219165
Jlevbury <125767916+Jlevbury@users.noreply.github.com>
2023-04-25 14:12:47 -0400

    pushing current state to request assistance from askBCS

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,8 @@ var apiKey = "826f633421f0289a9fa069f465862d53"
 var cityName = document.querySelector("#location").value;
 //var response = response.weatherReturned
 let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+let fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+console.log(fiveDayURL);
 var date = document.querySelector("#date");
 var temp = document.querySelector("#temp");
 var wind = document.querySelector("#wind");
@@ -13,6 +15,7 @@ console.log(date);
 //fetch(`${API_BASE_URL}weather?q=${cityName}&appid=${apiKey}`);
 //console.log(fetch);
 var submitBtn = document.querySelector("#submit");
+var ddFiveDay = document.querySelector("#ddFiveDay")
 var locationInput = document.querySelector("#location");
 var current = document.getElementById("#current");
 console.log(current);
@@ -30,6 +33,9 @@ $(document).ready(function() {
   displayDate();
 });
 
+
+
+
 // Add event listener to the submit button
 submitBtn.addEventListener("click", () => {
   
@@ -39,6 +45,9 @@ submitBtn.addEventListener("click", () => {
     // Build the query URL with the location input value and API key
     var apiKey = "826f633421f0289a9fa069f465862d53";
     var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=imperial`;
+
+    
+    
     
    // Call the fetch function with the query URL
 fetch(queryURL)
@@ -49,6 +58,8 @@ fetch(queryURL)
     wind= data.wind.speed;
     icon= data.weather[0].icon;
     city = data.name;
+    date = data.dt;
+    console.log(date);
 
     // create parent element
     var weatherData = document.createElement("div");
@@ -86,3 +97,18 @@ fetch(queryURL)
   });
 
   });
+  ddFiveDay.addEventListener("click", () => {
+    var location = locationInput.value;
+    var apiKey = "826f633421f0289a9fa069f465862d53";
+    let fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+  
+    fetch(fiveDayURL)
+    .then(response => response.json())
+    .then(data => {
+     console.log(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  })
+  
