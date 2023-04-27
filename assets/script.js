568c6d1d623a0737fc976893b3b5c66486694912
Jlevbury <125767916+Jlevbury@users.noreply.github.com>
2023-04-27 14:25:35 -0400

    pushing mvp state of project

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 var apiKey = "826f633421f0289a9fa069f465862d53"
 var cityName = document.querySelector("#location").value;
-//var response = response.weatherReturned
-let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-let fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+
+let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
+console.log(queryURL);
+let fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey+ "&units=imperial";
 console.log(fiveDayURL);
 var date = document.querySelector("#date");
 var temp = document.querySelector("#temp");
@@ -12,8 +13,7 @@ console.log(hum);
 console.log(wind);
 console.log(temp);
 console.log(date);
-//fetch(`${API_BASE_URL}weather?q=${cityName}&appid=${apiKey}`);
-//console.log(fetch);
+
 var submitBtn = document.querySelector("#submit");
 var ddFiveDay = document.querySelector("#ddFiveDay")
 var locationInput = document.querySelector("#location");
@@ -34,12 +34,17 @@ $(document).ready(function() {
   displayStoredCities();
 });
 
-function getCurrentForecast(city) {
-   var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-
+function getCurrentForecast(cityName) {
+  
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey+ "&units=imperial";
+ 
   fetch(queryURL)
     .then(response => response.json())
     .then(data => {
+      // clear existing contents of #current
+      var currWeather = document.querySelector("#current");
+      currWeather.innerHTML = "";
+
       temp = data.main.temp;
       console.log(temp);
       hum = data.main.humidity;
@@ -48,13 +53,44 @@ function getCurrentForecast(city) {
       city = data.name;
       date = data.dt;
       console.log(date);
+
+      // create parent element
+      var weatherData = document.createElement("div");
+      weatherData.setAttribute("id", "weather-data");
+
+      // create child elements
+      var tempEl = document.createElement("p");
+      tempEl.innerHTML = "Temperature: " + temp;
+
+      var humEl = document.createElement("p");
+      humEl.innerHTML = "Humidity: " + hum;
+
+      var windEl = document.createElement("p");
+      windEl.innerHTML = "Wind Speed: " + wind;
+
+      var iconEl = document.createElement("img");
+      iconEl.src = "http://openweathermap.org/img/w/" + icon + ".png";
+
+      var cityEl = document.createElement("p");
+      cityEl.innerHTML = "City: " + city;
+
+      // append child elements to parent element
+      weatherData.appendChild(tempEl);
+      weatherData.appendChild(humEl);
+      weatherData.appendChild(windEl);
+      weatherData.appendChild(iconEl);
+      weatherData.appendChild(cityEl);
+
+      // append parent element to currWeather
+      currWeather.appendChild(weatherData);
     })
     .catch(error => {
       console.error(error);
     });
 }
-function getFiveDayForecast(city) {
-  var fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+
+function getFiveDayForecast(cityName) {
+  var fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey+ "&units=imperial";
 
 fetch(fiveDayURL)
   .then(response => response.json())
@@ -133,16 +169,15 @@ submitBtn.addEventListener("click", () => {
     storedCities.push(location);
     localStorage.setItem("cities", JSON.stringify(storedCities));
   }
-    
     var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=imperial`;
-
-    
-    
     
    // Call the fetch function with the query URL
 fetch(queryURL)
   .then(response => response.json())
   .then(data => {
+    // clear existing contents of #current
+var currWeather = document.querySelector("#current");
+currWeather.innerHTML = "";
     temp = data.main.temp;
     console.log(temp);
     hum = data.main.humidity;
@@ -228,57 +263,47 @@ fetch(queryURL)
   
         for (let i = 0; i < data.list.length; i += 8) {
           const day = data.list[i];
-  
+        
           const date = new Date(day.dt * 1000).toLocaleDateString();
           const temp = day.main.temp;
           const humidity = day.main.humidity;
           const windSpeed = day.wind.speed;
           const iconUrl = `http://openweathermap.org/img/w/${day.weather[0].icon}.png`;
-  
+        
           // create child elements
           var dayEl = document.createElement("div");
           dayEl.classList.add("day");
-  
+        
           var dateEl = document.createElement("p");
           dateEl.innerHTML = date;
-  
+        
           var iconEl = document.createElement("img");
           iconEl.src = iconUrl;
-  
+        
           var tempEl = document.createElement("p");
           tempEl.innerHTML = `Temperature: ${temp} &#8457;`;
-  
+        
           var humidityEl = document.createElement("p");
           humidityEl.innerHTML = `Humidity: ${humidity}%`;
-  
+        
           var windSpeedEl = document.createElement("p");
           windSpeedEl.innerHTML = `Wind Speed: ${windSpeed} mph`;
-  
+        
           // append child elements to day element
           dayEl.appendChild(dateEl);
           dayEl.appendChild(iconEl);
           dayEl.appendChild(tempEl);
           dayEl.appendChild(humidityEl);
           dayEl.appendChild(windSpeedEl);
-  
+        
           // append day element to parent element
           forecastData.appendChild(dayEl);
         }
-  
-        // remove any existing forecast data
-        var existingForecastData = document.querySelector("#forecast-data");
-        if (existingForecastData) {
-          existingForecastData.remove();
-        }
-  
-        // append parent element to forecast section
-        var forecastSection = document.querySelector("#forecast");
-        forecastSection.appendChild(forecastData);
+        
       })
       .catch(error => {
         console.error(error);
       });
-      console.log("API response:", data);
 
   });
   
